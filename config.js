108b7f8126b221bcaeb6592564227786b2a9395a
Kontroller38 <successfulkontroller@gmail.com>
2024-12-18 10:03:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 ///=================When editing, do not accidentally delete anything !!MAIN-CONFIG=================================
-SESSION_ID: process.env.SESSION_ID || "LRdVQKSb#rWWMT_zd1cCwXF2bIsnskcYRpK0RgxD3RwJlJLo0H1A",  // ADD Your Session Id 
+SESSION_ID: process.env.SESSION_ID || "aFM3kRBD#Qq2VWWEgn0b8Ns_yGlllKXXf-G0EBEpRCpRePgf22IA",  // ADD Your Session Id 
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
 BOT_NAME: process.env.BOT_NAME || "Kontroller",
 ///=================When editing, do not accidentally delete anything !!CUSTOM-SETTING-CONFIG=====================
-AUTO_VOICE: process.env.AUTO_VOICE || "true", //add true or false
-AUTO_STICKER: process.env.AUTO_STICKER || "true", //add true or false
-AUTO_REPLY: process.env.AUTO_REPLY || "true", //add true or false
+AUTO_VOICE: process.env.AUTO_VOICE || "false", //add true or false
+AUTO_STICKER: process.env.AUTO_STICKER || "false", //add true or false
+AUTO_REPLY: process.env.AUTO_REPLY || "false", //add true or false
 ANTI_LINK: process.env.ANTI_LINK || "true", //add true or false
 ANTI_BAD: process.env.ANTI_BAD || "true", //add true or false
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true", //add true or false
@@ -26,6 +26,5 @@ GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME",
 PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 ///=================When editing, do not accidentally delete anything !!ALIVE-CONFIG================================
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/gdvaaw.jpg", //add alive img url
 ALIVE_MSG: process.env.ALIVE_MSG || "How far na Pablo dey talk any update", //add alive msg
 };
